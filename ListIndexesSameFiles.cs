8d06858b57b37e3e98be431e2f39de5d65e08ea1
svs00x <svs00x@hotmail.com>
2020-08-19 19:34:12 +0300

    Добавил класс для увазки элементов без пар

---
+++ ListIndexesSameFiles.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FarSync2
+{
+    class ListIndexesSameFiles
+    {
+        public int AmountIndexes = 0;   // количество текущих индексов для одинаковых файлов, первый индекс только если не встретились другие файлы для перестановок
+        public List<int> ListIndexes;   // список с индексами одинаковых файлов
+
+        public void AddSourceIndex(int AddIndex, bool NoPair)
+        {
+            int count = ((AmountIndexes == 0) ? 1 : 0) + (NoPair ? 1 : 0); // записываем обязательно первый встречающийся элемент и добавляем элемент, у которого нет пары
+            for (int c = 1; c <= count; c++)
+            {
+                AmountIndexes++;
+                if (AmountIndexes > ListIndexes.Count)
+                    ListIndexes.Add(AddIndex);
+                else
+                    ListIndexes[AmountIndexes] = AddIndex;
+            }
+        }
+
+        public void ResetListIndexesSameFiles()
+        {
+            AmountIndexes = 0;
+        }
+    }
+}
